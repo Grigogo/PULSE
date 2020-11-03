e30dad7a67020e6bbf5a8e33a01c3eb0ca9f0d1e
Grigogo <Grigoriirozhkov@gmail.com>
2020-11-03 22:38:21 +1100

    add section class=catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-alert (1);
+const slider = tns({
+    controls: false,
+    nav: false,
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });

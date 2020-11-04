d3ac4b74334a388a106f63d478ba4ad6996b1da1
Grigogo <Grigoriirozhkov@gmail.com>
2020-11-04 22:21:31 +1100

    add media style

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,19 @@ const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
-    autoplay: false
+    autoplay: false,
+    responsive: {
+      576: {
+        edgePadding: -5,
+        edgeMargin: 20,
+        gutter: 0,
+        items: 1,
+        center: true,
+        autoHeight: true,
+        autoWidth: false,
+        speed: 500
+      }
+    }
   });
 
   document.querySelector('.prev').addEventListener('click', function () {
@@ -21,4 +33,18 @@ const slider = tns({
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
   });

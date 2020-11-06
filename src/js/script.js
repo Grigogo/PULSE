93012f6570e6f7f738bd35acfabe5dab474e0b6b
Grigogo <Grigoriirozhkov@gmail.com>
2020-11-06 19:15:07 +1100

    add number mask

--- src/js/script.js
+++ src/js/script.js
@@ -46,4 +46,57 @@ const slider = tns({
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-  });
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function () {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      });
+    });
+
+
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minLenght: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+          messages: {
+            name: {
+              required: "Пожалуйста введите свое имя",
+              minLenght: jQuery.validator.format("Введите минимум {0} символа")
+            },
+            phone: {
+              required: "Пожалуйста, введите свой номер телефона",
+              phone: "Введите корректный номер"
+            },
+            email: {
+              required: "Пожалуйста, введите свой e-mail",
+              email: "Введите корректный E-mail: name@mail.ru"
+            }
+          }
+      });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    });
